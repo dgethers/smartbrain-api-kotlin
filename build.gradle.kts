d3ff689de1d96cc8965646e0c98f07f0e39543cd
David Gethers <dgethers.jr@gmail.com>
2021-09-28 16:37:53 -0400

    Setup project to use KoTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.21"
-    id("groovy") 
     id("org.jetbrains.kotlin.kapt") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("io.micronaut.application") version "2.0.3"
+    id("io.micronaut.application") version "2.0.6"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
 }
 
@@ -17,7 +16,7 @@ repositories {
 
 micronaut {
     runtime("netty")
-    testRuntime("spock2")
+    testRuntime("kotest")
     processing {
         incremental(true)
         annotations("com.smartbrain.*")
@@ -25,22 +24,22 @@ micronaut {
 }
 
 dependencies {
-    kapt("io.micronaut:micronaut-http-validation")
-    implementation("io.micronaut:micronaut-http-client")
-    implementation("io.micronaut:micronaut-runtime")
-    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    kapt("io.micronaut:micronaut-http-validation:3.0.1")
+    implementation("io.micronaut:micronaut-http-client:3.0.1")
+    implementation("io.micronaut:micronaut-runtime:3.0.1")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime:3.0.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    implementation("io.micronaut:micronaut-validation")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.6")
+    implementation("io.micronaut:micronaut-validation:3.0.1")
 
-    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
 
 }
 
 
 application {
-    mainClass.set("com.smartbrain.ApplicationKt")
+    mainClass.set("com.example.ApplicationKt")
 }
 java {
     sourceCompatibility = JavaVersion.toVersion("1.8")
