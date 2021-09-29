578eefc1c2e469d01b00a8a4ba8c8fc596c5e41d
David Gethers <dgethers.jr@gmail.com>
2021-09-29 14:51:42 -0400

    Add GRPC to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.21"
     id("org.jetbrains.kotlin.kapt") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("io.micronaut.application") version "2.0.6"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
+    id("com.google.protobuf") version "0.8.14"
 }
 
 version = "0.1"
 group = "com.smartbrain"
+val protocVersion = "3.12.2"
+val grpcVersion = "1.30.0"
+val kotlinVersion = project.properties.get("kotlinVersion")
 
-val kotlinVersion=project.properties.get("kotlinVersion")
 repositories {
     mavenCentral()
 }
@@ -31,6 +37,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     implementation("io.micronaut:micronaut-validation:3.0.1")
+    implementation("io.grpc:grpc-kotlin-stub:0.1.2")
+    implementation("io.micronaut.grpc:micronaut-grpc-client-runtime:3.0.0")
     testImplementation("io.kotest:kotest-assertions-core")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.6")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
@@ -55,4 +63,39 @@ tasks {
             jvmTarget = "1.8"
         }
     }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/grpc",
+                "build/generated/source/proto/main/grpckt",
+                "build/generated/source/proto/main/java"
+            )
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${protocVersion}"
+    }
+/*
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${grpcKotlinVersion}"
+        }
+    }
+*/
+
+    /*generateProtoTasks {
+        all() * . plugins {
+            grpc {}
+            grpckt {}
+        }
+    }*/
 }
